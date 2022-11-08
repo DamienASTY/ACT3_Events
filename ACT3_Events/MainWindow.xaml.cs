6a597c21856754b49911535fe4b6e18cd64b86e7
200051 <200051@PCI42101>
2022-11-08 14:27:27 +0100

    Ajoutez des fichiers projet.

---
+++ ACT3_Events/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+namespace ACT3_Events
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int[] Numbers = new int[3];
+        DispatcherTimer timer = new DispatcherTimer();
+        Math math = new Math();
+        string type = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+            //TextCompositionEventHandler PreviewTextInput;
+            A.PreviewTextInput += new TextCompositionEventHandler(VTI);
+            B.PreviewTextInput += new TextCompositionEventHandler(VTI);
+            C.PreviewTextInput += new TextCompositionEventHandler(VTI);
+
+            calc.MouseEnter += new MouseEventHandler(srvVBtn);
+            calc.MouseLeave += new MouseEventHandler(stopSrvVBtn);
+
+
+        }
+
+        private void Calculer(object sender, MouseButtonEventArgs e)
+        {
+             math.trimone(int.Parse(A.Text), int.Parse(B.Text), int.Parse(C.Text), out type);
+             PageResultat secondpage = new PageResultat();
+             secondpage.txtResultat.Text = type;
+             Visibility = Visibility.Hidden;
+        }
+
+        private void VTI(object sender, TextCompositionEventArgs e)
+        {
+            if (e.Text != "," && !EstEntier(e.Text))
+            {
+                e.Handled = true;
+            }
+            if (((TextBox)sender).Text.IndexOf(e.Text) > -1)
+            {
+                e.Handled = true;
+            }
+            else
+            {
+
+
+            }
+        }
+
+        private bool EstEntier(string userText)
+        {
+            return int.TryParse(userText, out _);
+        }
+
+        private void srvVBtn(object sender, EventArgs e)
+        {
+            virtualBtn.Visibility = Visibility.Visible;
+            virtualBtn.Background = Brushes.Red;
+        }
+
+        private void stopSrvVBtn(object sender, EventArgs e)
+        {
+            virtualBtn.Visibility = Visibility.Hidden;
+        }
+
+    }
+}
---
+++ ACT3_Events/Math.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ACT3_Events
+{
+    internal class Math
+    {
+        public void trimone(int a, int b, int c, out string type)
+        {
+            type = null;
+            double d;
+            double x1;
+            double x2;
+
+            d = System.Math.Pow(b, 2) - 4 * a * c;
+            if(d >= 0)
+            {
+                if(d > 0)
+                {
+                    x1 = (-b + System.Math.Sqrt(d) / (-2 * a));
+                    x2 = (-b - System.Math.Sqrt(d) / (-2 * a));
+                    type = "Il y a deux racines réelles :" + x1 + " et " + x2; 
+                }
+                else
+                {
+                    x1 = (-b) / (2 * a);
+                    type = "Il y a une seule racine : "  + x1;
+                }
+            }
+            else
+            {
+                type = "Il n'y a pas de racine";
+            }
+        }
+    }
+    
+}
